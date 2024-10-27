a118bcf3aee6bae0dea94c5ee31512f4ceb95e51
codeonline26 <132570906+codeonline26@users.noreply.github.com>
2024-10-27 10:12:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func init() {
 	d := flag.Bool("d", false, "Enable debug level log and higher.")
 	w := flag.Bool("w", false, "Enable warning level log and higher.")
 	h := flag.Bool("h", false, "Display this help.")
-	// g := flag.String("g", "127.0.0.1:3000", "Set webui url.")
+        g := flag.String("g", "127.0.0.1:3000", "Set webui url.")
 	// 直接写死 AccessToken 时，请更改下面第二个参数
 	token := flag.String("t", "", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
